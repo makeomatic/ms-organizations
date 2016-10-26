a080a38b40f1eab94f225481eaf8c553b058ed77
Aleksandr Sorokin <imarkoffko@gmail.com>
2016-10-26 14:51:24 +0300

    feat(organizations): get action

--- test/suites/organizations.get.js
+++ test/suites/organizations.get.js
@@ -1,16 +1,17 @@
 const assert = require('assert');
 const Chance = require('chance');
 const config = require('../configs/service');
+const isISODate = require('../helpers/asserts/ISODate');
 const Service = require('../../src');
 
 const chance = new Chance();
 const service = new Service(config);
 
-describe('organizations.delete', function suite() {
+describe('organizations.get', function suite() {
   before('starting service', () => service.connect());
   after('stoping service', () => service.close());
 
-  it('should be able to delete organization', () => {
+  it('should be able to get organization', () => {
     const { organization: organizationService } = service.services;
     const params = {
       name: chance.name(),
@@ -21,10 +22,17 @@ describe('organizations.delete', function suite() {
       .create(params)
       .then(organization =>
         service.amqp
-          .publishAndWait('organizations.organizations.delete', { id: organization.id })
+          .publishAndWait('organizations.organizations.get', { id: organization.id })
       )
       .then((response) => {
-        assert.deepEqual(response, { status: 'success' });
+        assert.ok(response.alias);
+        assert.equal(isISODate(response.createdAt), true);
+        assert.equal(response.enabled, false);
+        assert.ok(response.id);
+        assert.deepEqual(response.meta, {});
+        assert.ok(response.name);
+        assert.equal(response.ownerId, 'foo@bar.com');
+        assert.equal(isISODate(response.updatedAt), true);
       });
   });
 });
